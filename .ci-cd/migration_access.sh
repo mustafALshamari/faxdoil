3eedcb3e22afd494fbca90652b9b030517c68d5c
Yurii Aleshchenko <y.aleshchenko@newlinetechnologies.net>
2020-04-28 12:44:26 +0300

    add several command artisan

--- .ci-cd/migration_access.sh
+++ .ci-cd/migration_access.sh
@@ -5,4 +5,7 @@ chmod -R ug+rwx /var/www/api/storage /var/www/api/bootstrap/cache
 chgrp -R www-data /var/www/api/storage /var/www/api/bootstrap/cache
 chmod -R ug+rwx /var/www/api/storage /var/www/api/bootstrap/cache
 
+php /var/www/api/artisan migrate
 php /var/www/api/artisan key:generate
+php /var/www/api/artisan passport:install
+php /var/www/api/artisan passport:client --personal
