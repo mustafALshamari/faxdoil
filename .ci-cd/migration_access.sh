4aeecc94773ca31b5ee66e5c44e3ae764d269e4d
y.aleshchenko <y.aleshchenko@newlinetechnologies.net>
2020-05-05 09:40:04 +0000

    Update migration_access.sh

--- .ci-cd/migration_access.sh
+++ .ci-cd/migration_access.sh
@@ -7,7 +7,10 @@ chmod -R ug+rwx /var/www/api/storage /var/www/api/bootstrap/cache
 
 php /var/www/api/artisan key:generate
 php /var/www/api/artisan passport:install
-php /var/www/api/artisan migrate
+php /var/www/api/artisan migrate:refresh
 php /var/www/api/artisan passport:client --personal
 php /var/www/api/artisan db:seed --class=AdminTableDataSeeder
 php /var/www/api/artisan l5-swagger:generate
+php /var/www/api/artisan artisan cache:clear 
+php /var/www/api/artisan artisan view:clear 
+php /var/www/api/artisan artisan route:cache
