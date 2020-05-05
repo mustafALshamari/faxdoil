fce32749c64ad173a56eab91702df0ce924b8252
[DevOps] Yurii Aleshchenko <y.aleshchenko@newlinetechnologies.net>
2020-05-05 07:55:38 -0400

    change permission for log folder

--- .ci-cd/migration_access.sh
+++ .ci-cd/migration_access.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 chown -R root:www-data /var/www/api
-chmod -R ug+rwx /var/www/api/storage /var/www/api/bootstrap/cache
+chmod -R 775 /var/www/api/storage /var/www/api/bootstrap/cache
 chgrp -R www-data /var/www/api/storage /var/www/api/bootstrap/cache
-chmod -R ug+rwx /var/www/api/storage /var/www/api/bootstrap/cache
 
 php /var/www/api/artisan key:generate
 php /var/www/api/artisan passport:install
